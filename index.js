3749213c94ee4de1d8493edd7add4a675cc9b467
Ayim William <odhisayim@gmail.com>
2024-03-02 16:37:06 +0300

    Finished first function

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code here
+function calculateHours(startStamp, endStamp){
+    const start = new Date(startStamp);
+    const end = new Date(endStamp);
+    const diffMs = end-start;
+    const diffHrs = diffMs / (1000 * 60 * 60); // convert milliseconds to hours
+    return Math.abs(diffHrs); 
+}
