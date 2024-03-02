bfe954e68fc6e921efdc6a1d1e4968419820c5dc
Ayim William <odhisayim@gmail.com>
2024-03-02 17:28:47 +0300

    Added two more functions

--- index.js
+++ index.js
@@ -6,3 +6,28 @@ function calculateHours(startStamp, endStamp){
     const diffHrs = diffMs / (1000 * 60 * 60); // convert milliseconds to hours
     return Math.abs(diffHrs); 
 }
+
+function createEmployeeRecord(employeeData) {
+    return {
+        firstName: employeeData[0],
+        familyName: employeeData[1],
+        title: employeeData[2],
+        payPerHour: employeeData[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+}
+
+function createEmployeeRecords(arrays) {
+    return arrays.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(employee, dateStamp) {
+    const [date, time] = dateStamp.split(' ');
+    const [year, month, day] = date.split('-');
+    const hour = time.slice(0,2);
+    const minute = time.slice(2);
+    const dateTime  = new Date(year, month-1, day, hour, minute);
+    employee.timeInEvents.push({type: 'TimeIn', hour: parseInt(hour + minute), date });
+    return employee;
+}
